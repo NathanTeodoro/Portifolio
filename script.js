4b3ea62deae4d2472e2a2813ab82aa4ee2858525
Nathan Teodoro <nathan992st@gmail.com>
2023-05-21 09:10:45 -0300

    ten commit

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let icss = document.querySelector(".a_css")
 let inode = document.querySelector(".a_node")
 let ipython = document.querySelector(".a_python")
 
-const showdiv = "showdiv"
-const hidediv = "hidediv"
+const hidediv = "hidediv";
+const showdiv = "showdiv";
 
 let a_js = icon[0];
 let a_html = icon[1]; 
@@ -17,39 +17,23 @@ let a_python = icon[4];
 
 
 a_js.addEventListener('click', ()=>{
-    ijs.classList.add(showdiv)
-    setTimeout(a_js.addEventListener('click', ()=>{
-        ijs.classList.remove(showdiv)
-    }),1)
-   
+    ijs.classList.toggle(showdiv)
 });
 
 
 a_html.addEventListener('click', ()=>{
-    ihtml.classList.add(showdiv)
-    setTimeout(a_html.addEventListener('click', ()=>{
-        ihtml.classList.remove(showdiv)
-    }),1)
+   ihtml.classList.toggle(showdiv)
 });
 
 a_css.addEventListener('click', ()=>{
-    icss.classList.add(showdiv)
-    setTimeout(a_css.addEventListener('click', ()=>{
-        icss.classList.remove(showdiv)
-    }),1)
+   icss.classList.toggle(showdiv)
 });
 
 a_node.addEventListener('click', ()=>{
-    inode.classList.add(showdiv)
-    setTimeout(a_node.addEventListener('click', ()=>{
-        inode.classList.remove(showdiv)
-    }),1)
+    inode.classList.toggle(showdiv)
 });
 
 a_python.addEventListener('click', ()=>{
-    ipython.classList.add(showdiv)
-    setTimeout(a_python.addEventListener('click', ()=>{
-        ipython.classList.remove(showdiv)
-    }),1)
+    inode.classList.toggle(showdiv)
 });
 
